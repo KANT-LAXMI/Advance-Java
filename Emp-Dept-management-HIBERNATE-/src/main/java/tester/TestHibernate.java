2b1897d67d3beaee28ab83b990068367af8eccb2
LAXMI KANT <130977953+KANT-LAXMI@users.noreply.github.com>
2023-08-06 01:00:41 +0530

    Update TestHibernate.java

--- Emp-Dept-management-HIBERNATE-/src/main/java/tester/TestHibernate.java
+++ Emp-Dept-management-HIBERNATE-/src/main/java/tester/TestHibernate.java
@@ -7,7 +7,7 @@ public class TestHibernate {
 
 	public static void main(String[] args) {
  
-		//cls loading --> static init block --> configure : entire hib frmwork loaded
+	
 		try(SessionFactory sf=getFactory()){
 			System.out.println("hib up n running !!!! "+sf);
 		} //sf.close() --> cleaning up DBCP (db conn pool)
